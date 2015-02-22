544b343254ca1de322652c0f814695e23d561aeb
Joe Dister <joedister@gmail.com>
2015-02-22 14:18:36 -0500

    Initial Commit

---
+++ KSUAdvising/Controllers/AdvisorController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KSUAdvising.Controllers
+{
+    public class AdvisorController : Controller
+    {
+        //
+        // GET: /Advisor/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
