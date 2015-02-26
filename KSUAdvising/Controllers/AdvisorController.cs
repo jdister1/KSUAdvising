e78505b7eb1fc14f26f59268848da0ef02c9f142
unknown <joedister@gmail.com>
2015-02-25 22:58:59 -0500

    Adding EF support

--- KSUAdvising/Controllers/AdvisorController.cs
+++ KSUAdvising/Controllers/AdvisorController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using KSUAdvising.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,9 +11,18 @@ namespace KSUAdvising.Controllers
     {
         //
         // GET: /Advisor/
-
+        //AdvisingDBContext context = new AdvisingDBContext();
         public ActionResult Index()
         {
+            //Adviser firstAdviser = new Adviser();
+            //firstAdviser.AdvisorID = 123;
+            //firstAdviser.FirstName = "Joe";
+            //firstAdviser.LastName = "Dister";
+            //context.Advisers.Add(firstAdviser);
+
+            //context.SaveChanges();
+
+            //List<Adviser> advisers = context.Advisers.ToList();
             return View();
         }
 
---
+++ KSUAdvising/Models/Adviser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace KSUAdvising.Models
+{
+    [Table("Advisers")]
+    public class Adviser
+    {
+        [Key]
+        public int AdvisorID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
